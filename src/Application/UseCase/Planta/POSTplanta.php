1bbe8a9c875be5b5449042b210f4b3b531e18e16
mwly-01 <mlzardila@gmail.com>
2025-08-11 13:22:01 +0000

    feat: :sparkles: se esta diseñando el apartado de crear

--- src/Application/UseCase/Planta/POSTplanta.php
+++ src/Application/UseCase/Planta/POSTplanta.php
@@ -1,6 +1,22 @@
 <?php
 
-namespace App\useCase;
+namespace App\Application\UseCase\Planta;
+
+use App\Application\Dtos\Contracts\ArraySerializableDto;
 
 use App\Domain\Repository\PlantaRepository;
-use App\Domain\Model\Planta;
+
+class CreatePlantaUseCase
+{
+    public function __construct(private PlantaRepository $repository) {}
+
+    /**
+     * @param ?ArraySerializableDto $dto
+     * @return mixed
+     */
+         public function __invoke(?ArraySerializableDto $dto = null)
+    {
+         return $this->repository->createPlanta($dto->toArray());
+     }
+
+}
